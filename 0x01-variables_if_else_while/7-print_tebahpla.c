d8113786af15b64fc8412784c05c6b892f5bd730
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-06-21 14:22:18 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - My main function goes here
+*
+* Return: 0 if succeed
+*/
+int main(void)
+{
+	char a = 'z';
+
+	while
+		(a >= 'a')
+		{
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
