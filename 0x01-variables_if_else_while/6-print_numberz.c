ce4ae318d07e750c506ab6669a745371c02c6023
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-06-21 14:18:14 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - My main function goes here
+ *
+ * Return: 0 if succeed
+ */
+int main(void)
+{
+	int a = 0;
+
+	while
+		(a < 10)
+		{
+			putchar(a + '0');
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
