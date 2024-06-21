3fa7ea9588037d9d8f024f5170f41360299ff366
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-06-21 13:45:50 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - My main function goes here
+ *
+ * Return: 0 if succeed
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
