98dc25bab978a4a026926a22ab874308797f5d16
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-06-21 14:12:47 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - My main function goes here
+ *
+ * Return: 0 if succeed
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
