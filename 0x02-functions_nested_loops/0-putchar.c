5a44a846d2364253b2379a54903fd8d44cad4917
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-06-25 12:44:45 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - My main function goes here
+ * main - My main function goes here
  *
  * Return: 0 if succeed
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
