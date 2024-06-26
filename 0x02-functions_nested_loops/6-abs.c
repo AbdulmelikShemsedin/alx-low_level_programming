fd0343c8991cbc4b871e9f449253de3645d1638b
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-06-26 08:02:05 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - My main function goes here
+ *
+ * Return: abs if succeed: c otherwise
+ */
+int _abs(int)
+{
+	if (c < 0)
+	{
+		int abs;
+
+		abs = c * -1;
+		return (abs);
+	}
+	return (c);
+}
