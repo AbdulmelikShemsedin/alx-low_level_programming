d8100f17743bde9e912fb3416b73aa32a032d266
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-06-26 07:21:06 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,14 @@
 
 /**
  * _islower - My main function goes here
-  *@c: checking character
+ * @c: checking character
  * Return: 1 if c is lowercase: 0 otherwise
  */
 int _islower(int c)
 {
 	char a = c;
 
-	if (a == tolower(a))
+	if (c >= 97 && a == tolower(a))
 	{
 		return (1);
 	}
