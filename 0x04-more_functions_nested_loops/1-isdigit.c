82712dcde1197eea738991229a5841add88ed56f
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-06-27 14:38:59 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - My main func goes here
+ * @x: checker
+ * Return: 1 if digit: 0 otherwise
+ */
+int _isdigit(int x)
+{
+	if (x >= 48 && x <=57)
+	{
+		return (1);
+	}
+	return (0);
+}
