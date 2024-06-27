6e943652ef22a87c73c2b89d97ff58e3c2d82109
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-06-27 14:45:48 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - My function goes here
+ * @x: first one
+ * @y: second one
+ * Return: the result of multiplication
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
