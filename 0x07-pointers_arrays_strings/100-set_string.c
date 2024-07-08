0a62c97b0b023bc072d8fad76709dde8b1c422c2
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-07-08 07:11:55 -0700

    advanced

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - My main func goes here
+ * @s: checker input
+ * @to: checker input
+ * void: sets string
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
