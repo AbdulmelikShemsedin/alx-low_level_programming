07014fc3995e0758c408a7e22637ae2ff1ef361f
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-07-15 08:29:58 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,16 +9,20 @@
 char *_strdup(char *str)
 {
 	char *newStr;
+	int str_len, i = 0;
 
+	for (; str[i] != '\0'; i++)
+		str_len++;
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	newStr = (char *)malloc(sizeof(str));
+	newStr = (char *)malloc((str_len + 1) * sizeof(char));
 	if (newStr == NULL)
 	{
 		return (NULL);
 	}
-	newStr = str;
+	for (i = 0; i < (str_len + 1); i++)
+		newStr[i] = str[i];
 	return (newStr);
 }
