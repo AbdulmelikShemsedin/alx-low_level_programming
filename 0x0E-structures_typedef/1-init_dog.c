003c991a116d87a6f60551ca1bc55cb78791c85f
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-07-22 08:55:22 -0700

    paused

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize a variable
+ * @d: pointer to initialize
+ * @name: variable in pointer
+ * @age: variable in pointer
+ * @owner: variable in pointer
+ * void: initialize var
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
