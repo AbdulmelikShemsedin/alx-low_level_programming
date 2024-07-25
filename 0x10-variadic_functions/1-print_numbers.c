576e11e4a0d613020f58437e9116bf564acb55e9
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-07-25 08:53:52 -0700

    print nos

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ * @separator: string to be printed bet/n nos
+ * @n: no of ints passing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list no;
+unsigned int i;
+
+va_start(no, n);
+for (i = 0; i < n - 1; i++)
+{
+printf("%d%s", va_arg(no, int), separator);
+}
+printf("%d\n", va_arg(no, int));
+va_end(no);
+}
