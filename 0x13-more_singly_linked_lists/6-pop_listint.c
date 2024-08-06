123976730addd41b7ea06d5e85cc3953dd7f3ff4
Abdulmelik Shemsedin <abdulmelikshemsedin@gmail.com>
2024-08-06 12:32:45 -0700

    freeing list again

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of list
+ * @head: pointer to the head
+ * Return: the head node's data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int new;
+
+	if (*head == NULL)
+		return (NULL);
+	temp = *head;
+	new = temp->n;
+	*head = (*head)->next;
+	free(temp);
+	return (new);
+}
