5ec0218cf0ba3ffad1a506f8ce25d7359b407bfc
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2024-09-19 22:09:37 +0300

    styled to betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,23 +1,28 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints elements of the list
+ * @h: the list
+ * Return: the lists count
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int c = 0;
+	int c = 0;
 
-    if (h == NULL)
-    {
-        return (c);
-    }
-    while (h->prev != NULL)
-    {
-        h = h->prev;
-    }
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        c++;
-        h = h->next;
-    }
+	if (h == NULL)
+	{
+		return (c);
+	}
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		c++;
+		h = h->next;
+	}
 
-    return (c);
+	return (c);
 }
