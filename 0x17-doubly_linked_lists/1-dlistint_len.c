e33ec7e68713772a46e7f1114a492d42fd402421
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2024-09-19 22:25:16 +0300

    styled for betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,20 +7,20 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    int c = 0;
+int c = 0;
 
-    if (h == NULL)
-    {
-        return (c);
-    }
-    while (h->prev != NULL)
-    {
-        h = h->prev;
-    }
-    while (h != NULL)
-    {
-        c++;
-        h = h->next;
-    }
-    return (c);
+if (h == NULL)
+{
+return (c);
+}
+while (h->prev != NULL)
+{
+h = h->prev;
+}
+while (h != NULL)
+{
+c++;
+h = h->next;
+}
+return (c);
 }
